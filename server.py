c90d82d33a843746a9b42d07284e23f21e3968fe
ande1000 <aguiaprodutorprodutor@gmail.com>
2026-06-25 19:56:57 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-sdk = mercadopago.SDK("APP_USR-5736314269671526-062323-4c8cc89d1112ac6799a6d37e85009840-750814888")
+sdk = mercadopago.SDK("APP_USR-5736314269671526-062323-4c8cc89d1112ac6799a6d3")
 
 @app.route('/')
 def index():
@@ -13,7 +13,7 @@ def index():
 @app.route('/processar_pagamento', methods=['POST'])
 def processar_pagamento():
     dados = request.json
-    
+
     payment_data = {
         "transaction_amount": float(dados['transaction_amount']),
         "description": dados['description'],
@@ -25,7 +25,12 @@ def processar_pagamento():
 
     result = sdk.payment().create(payment_data)
     
-    return jsonify(result["response"]), result["status"]
+    response_data = result.get("response")
+    qr_code = None
+    if response_data and "point_of_interaction" in response_data:
+        qr_code = response_data["point_of_interaction"]["transaction_data"]["qr_code"]
+
+    return jsonify({"response": response_data, "qr_code": qr_code}), result["status"]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
